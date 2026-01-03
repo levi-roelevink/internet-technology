c7c4d57ab57caaa05ea5c835dbf5130a663380e9
Levi Roelevink <538253@student.saxion.nl>
2026-01-03 10:55:27 +0100

    Tests for continuous communication

--- test/TestGreeting.java
+++ test/TestGreeting.java
@@ -1,4 +1,6 @@
 import client.Client;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -6,17 +8,27 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGreeting {
+    public static Client client;
+
+    @BeforeAll
+    public static void setup() throws IOException {
+        client = new Client();
+        client.startConnection("127.0.0.1", 3000);
+    }
+
+    @AfterAll
+    public static void tearDown() throws IOException {
+        client.stopConnection();
+    }
+
     @Test
     public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
-        Client client = new Client();
-        client.startConnection("127.0.0.1", 3000);
-        String response = client.sendMessage("Hello server");
-        assertEquals("Hello server", response);
+        String resp1 = client.sendMessage("Hello server");
+        String resp2 = client.sendMessage("I am Bob");
+        String resp3 = client.sendMessage(".");
 
-        response = client.sendMessage("I am Bob");
-        assertEquals("I am Bob", response);
-
-        response = client.sendMessage(".");
-        assertEquals("Goodbye", response);
+        assertEquals("Hello server", resp1);
+        assertEquals("I am Bob", resp2);
+        assertEquals("Goodbye", resp3);
     }
 }
