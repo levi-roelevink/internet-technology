8320e5910518f400df6ec567c4383ebd04742211
Levi Roelevink <538253@student.saxion.nl>
2026-01-03 10:27:14 +0100

    Changed class names to be more descriptive

--- src/client/Server.java
+++ src/client/Server.java
@@ -6,9 +6,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
-public class GreetServer {
+public class Server {
     private final static int PORT = 3000;
     private Socket clientSocket;
     private ServerSocket serverSocket;
@@ -38,7 +37,7 @@ public class GreetServer {
     }
 
     public static void main(String[] args) throws IOException {
-        GreetServer server = new GreetServer();
+        Server server = new Server();
         server.start(PORT);
     }
 }
--- test/TestGreeting.java
+++ test/TestGreeting.java
@@ -1,5 +1,4 @@
-import client.GreetClient;
-import client.GreetServer;
+import client.Client;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,9 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestGreeting {
     @Test
     public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
-        GreetClient client = new GreetClient();
+        Client client = new Client();
         client.startConnection("127.0.0.1", 3000);
         String response = client.sendMessage("Hello server");
         assertEquals("Hello client", response);
+
+
     }
 }
