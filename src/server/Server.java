986975264401b9ff26674e66c06192a5cb42b2d7
Levi Roelevink <538253@student.saxion.nl>
2026-01-08 14:54:11 +0100

    Started the migration of the server from JS to Java

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,11 +1,7 @@
 package server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
     private final static int PORT = 3000;
@@ -15,6 +11,7 @@ public class Server {
         serverSocket = new ServerSocket(port);
 
         while (true) {
+            // Wait for an incoming client-connection request (blocking)
             new ServerThread(serverSocket.accept()).start();
         }
     }
@@ -24,39 +21,6 @@ public class Server {
         while (true) {
             server.start(PORT);
             server.serverSocket.close();
-        }
-    }
-
-    private static class ServerThread extends Thread {
-        private Socket clientSocket;
-        private PrintWriter out;
-        private BufferedReader in;
-
-        public ServerThread(Socket socket) {
-            this.clientSocket = socket;
-        }
-
-        public void run() {
-            try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    System.out.printf("Received \"%s\"\n", inputLine);
-                    if (".".equals(inputLine)) {
-                        out.println("Goodbye");
-                        break;
-                    }
-                    out.println(inputLine);
-                }
-
-                in.close();
-                out.close();
-                clientSocket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
---
+++ src/server/ServerThread.java
@@ -0,0 +1,92 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+    private final Socket socket;
+    private PrintWriter writer;
+    private BufferedReader in;
+    private final String PONG = "PONG";
+    private final String BYE = "BYE";
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            PingThread pingThread = new PingThread(socket, writer);
+            pingThread.start();
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.printf("Received \"%s\"\n", inputLine);
+                String[] lineParts = inputLine.split(" ", 2);
+
+                switch (lineParts[0]) {
+                    case PONG -> pingThread.setAwaitingPong(false);
+                    case BYE -> terminateConnection();
+                    default -> System.out.println("Unknown command...");
+                }
+
+                writer.println(inputLine);
+            }
+
+            in.close();
+            writer.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void terminateConnection() {
+        System.out.println("BEEP BOOP CAT POOP");
+    }
+
+    private class PingThread extends Thread {
+        private final Socket socket;
+        private final PrintWriter writer;
+        private final int PING_DELAY_MS = 10_000;
+        private final String PING = "PING";
+        private boolean awaitingPong = true;
+
+        PingThread(Socket socket, PrintWriter writer) {
+            this.socket = socket;
+            this.writer = writer;
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    sleep(PING_DELAY_MS);
+
+                    if (awaitingPong) {
+                        // TODO: no PONG response within 10_000 MS, hence the client has lost connection
+                        socket.close();
+                        writer.close();
+                        throw new Exception("No pong received.");
+                    }
+                    writer.println(PING);
+                } catch (Exception e) {
+                    throw new RuntimeException(e.getMessage());
+                }
+            }
+        }
+
+        protected boolean getAwaitingPong() {
+            return awaitingPong;
+        }
+
+        protected void setAwaitingPong(boolean awaiting) {
+            awaitingPong = awaiting;
+        }
+    }
+}
