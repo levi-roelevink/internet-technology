c08a6c026025b92a2652064a04c048b2d602331e
Levi Roelevink <538253@student.saxion.nl>
2026-01-13 12:05:03 +0100

    Fixed incorrect error code

--- src/client/MessageCodePrinter.java
+++ src/client/MessageCodePrinter.java
@@ -6,8 +6,8 @@ public interface MessageCodePrinter {
             case 1000 -> System.out.println("User has already logged in."); // User probeert in te loggen op een user die al ingelogd is
             case 1001 -> System.out.println("Username has an invalid length or format.");
             case 1002 -> System.out.println("User cannot login twice."); // User that is already logged in is trying to log in
+            case 2000 -> System.out.println("User is not logged in.");
             case 5000 -> System.out.println("Invalid recipient provided.");
-            case 6000 -> System.out.println("User is not logged in.");
             case 7000 -> System.out.println("Pong timeout.");
             case 7001 -> System.out.println("Unterminated message.");
             case 8000 -> System.out.println("Pong without ping.");
